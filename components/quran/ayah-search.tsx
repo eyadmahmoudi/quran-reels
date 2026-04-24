e92d90217d614994ebaad1d00fd916e69ef5ef58
Eyadmahmoudi <mohamed.eyad.mahmoudi@ensia.edu.dz>
2026-04-24 15:11:51 +0100

    your change description

--- components/quran/ayah-search.tsx
+++ components/quran/ayah-search.tsx
@@ -27,7 +27,7 @@ export function AyahSearch() {
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(async () => {
-      if (query.trim().length > 2) {
+      if (query.trim().length > 4) {
         setLoading(true);
         const searchResults = await searchAyahs(query);
         setResults(searchResults);
